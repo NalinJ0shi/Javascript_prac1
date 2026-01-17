b215f855da3c54f5b8475c0e854bf47758007016
Nalin Joshi <nalinjoshi629@gmail.com>
2026-01-17 19:34:11 +0530

    insertion sort

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // }
 let arr = [64, 34, 25, 12, 22, 11, 90];
 console.log("Unsorted array:", arr);
-let sortedArr = bubbleSort(arr);
+let sortedArr = insertionSort(arr);
 console.log("Sorted array:", sortedArr);
 function bubbleSort(arr) {
     let n = arr.length;
@@ -16,6 +16,18 @@ function bubbleSort(arr) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
+    }
+    return arr;
+}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
     }
     return arr;
 }
