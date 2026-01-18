eba0a75667c27314ac1951ec89f1c70468269a36
Nalin Joshi <nalinjoshi629@gmail.com>
2026-01-18 19:20:30 +0530

    Refactor main.js and add main3.js for sorting algorithms

--- main.js
+++ main.js
@@ -1,33 +1,7 @@
-//first day coding in my hp omnibook7
-// console.log("Hello, Omnibook7!");
+//first day practising javascript in Omnibook7
+console.log("Hello, Omnibook7!");
+//simple loop
 // let a = [1,2,3,4,5];
 // for(let i=0;i<5;i++){
 //     console.log(a[i]);
 // }
-let arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Unsorted array:", arr);
-let sortedArr = insertionSort(arr);
-console.log("Sorted array:", sortedArr);
-function bubbleSort(arr) {
-    let n = arr.length;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
-    }
-    return arr;
-}
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let key = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
-    return arr;
-}
---
+++ main3.js
@@ -0,0 +1,27 @@
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Unsorted array:", arr);
+let sortedArr = insertionSort(arr);
+console.log("Sorted array:", sortedArr);
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
